d258b54be302077171807271faee9648b11ea2bb
Femibams <fbamidele@terragonltd.com>
2018-07-03 15:50:43 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes Tutorial';
+  loader = true;
+
+  ngOnInit() {
+    this.delay();
+  }
+  // setTimeout(function(){ alert("Hello"); }, 3000);
+  delay(){
+    setTimeout(() => { 
+      this.loader = false; 
+    }, 3000);
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Femibams' },
+    { id: 13, name: 'Oseme' },
+    { id: 14, name: 'Ifiok' },
+    { id: 15, name: 'Mr Kass' }
+];
